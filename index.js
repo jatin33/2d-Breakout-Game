f31bfd1035dc7e747d307d87df439c4661a209bd
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-05-05 16:44:14 +0530

    Paddle movement logic added

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ const ctx = canvas.getContext("2d");
 const ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
+const paddleHeight = 15;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = canvas.height - paddleHeight;
 let dx = 2;
 let dy = -2;
+let leftPressed = false;
+let rightPressed = false;
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+
+function keyDownHandler(e) {
+    if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = true;
+    } else if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = false;
+    }
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = false;
+    }
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -14,17 +40,29 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
     drawBall();
-
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-
+    if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    } else if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }
     x += dx;
     y += dy;
 }
